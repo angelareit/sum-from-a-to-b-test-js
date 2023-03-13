972f4f4ec5601c002058152700c6a1d78bd5f94e
angelareit <anreit01@email.com>
2023-03-13 03:58:19 +0000

    added the recursive function on the sum function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let currSum = + fromN;
+
+  //base case (or exit condition)
+  if (fromN === toN) {
+    return currSum;
+  }
+  //recursive casegit add 
+  else {
+    return fromN + sum(++fromN, toN);
+  }
+
+
 }
 
 module.exports = sum;
